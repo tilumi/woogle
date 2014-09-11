ab1bd85dd4f5d4a8a8a6f2260f97addb5dda5e2d
Lucas Yang <lucasmf@yahoo-inc.com>
2014-09-12 06:28:24 +0800

    modify build process

--- build-prod.sh
+++ build-prod.sh
@@ -9,7 +9,7 @@ mkdir -p output/etc
 mkdir -p output/bin
 mkdir -p output/lib
 mkdir -p output/libexec
-mkdir -p /var/log/woogle
+sudo mkdir -p /var/log/woogle
 mkdir -p output/pid
 
 cp -r webapp output
